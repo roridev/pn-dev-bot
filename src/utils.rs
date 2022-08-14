e72a97ec648386ac842cbc5e5d7faf3e4b58f9ea
roridev <t3ctotalmenterandom1@outlook.com>
2022-08-14 14:50:27 -0300

    Add emoji module
    Contains useful emojis. Values are hardcoded based on my personal test
    discord. Easily changeable.

--- src/utils.rs
+++ src/utils.rs
@@ -19,5 +19,82 @@ pub fn format_it(issue: &Issue) -> String {
         format!(" {} ➠ `{}`", base, size.first().unwrap().name)
     } else {
         base
+pub mod emoji {
+    use std::fmt::Display;
+
+    pub enum PullRequest {
+        Open,
+        Closed,
+        Draft,
+        Merged,
+    }
+
+    impl Display for PullRequest {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let result = match self {
+                PullRequest::Open => "<:gh_pr_closed:1008308520669683732>",
+                PullRequest::Closed => "<:gh_pr_closed:1008308520669683732>",
+                PullRequest::Draft => "<:gh_pr_draft:1008308521814728756>",
+                PullRequest::Merged => "<:gh_pr_merged:1008308523341451374>",
+            };
+            write!(f, "{}", result)
+        }
+    }
+
+    pub enum Issue {
+        Open,
+        Closed,
+        NotPlanned,
+    }
+
+    impl Display for Issue {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let result = match self {
+                Issue::Open => "<:gh_issue_open:1008308515527471104>",
+                Issue::Closed => todo!(),
+                Issue::NotPlanned => todo!(),
+            };
+            write!(f, "{}", result)
+        }
+    }
+
+    pub enum Action {
+        Refresh,
+        Copy,
+        Updated,
+        OpenLink,
+        Error,
+    }
+
+    impl Display for Action {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let result = match self {
+                Action::Refresh => "<:gh_refresh:1008341077314515014>",
+                Action::Copy => "<:gh_copy:1008341074508521594>",
+                Action::Updated => "<:gh_updated:1008308527913259028>",
+                Action::OpenLink => "<:gh_open_link:1008308518908084277>",
+                Action::Error => "<:gh_error:1008308514302722200>",
+            };
+            write!(f, "{}", result)
+        }
+    }
+
+    pub enum Misc {
+        File,
+        Tag,
+        Lock,
+        LeftArrow,
+    }
+
+    impl Display for Misc {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let result = match self {
+                Misc::File => "<:gh_file:1008341075980726312>",
+                Misc::Tag => "<:gh_tag:1008308526281670666>",
+                Misc::Lock => "<:gh_lock:1008308517763039273>",
+                Misc::LeftArrow => "<:gh_left_arrow:1008308516794142720>",
+            };
+            write!(f, "{}", result)
+        }
     }
 }
