64f72d5e05234948c32d2e1c915faff5b8e26bc7
roridev <prates.aliceic@gmail.com>
2022-08-15 20:42:17 -0300

    [Broken] Add base bot code setup
    The code itself doesn't run due to a type parameter being elided on
    the framework definition.

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+pub mod config;
+
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Config, Error>;
+
+use config::Config;
+use poise::serenity_prelude as serenity;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let config = config::read_from_file("./config.json")?;
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![],
+            ..Default::default()
+        })
+        .token(config.tokens.discord_token)
+        .intents(serenity::GatewayIntents::non_privileged())
+        .user_data_setup(move |_ctx, _ready, _framework| Box::pin(async move { Ok(config) }));
+
+    framework.run().await.unwrap();
+
+    Ok(())
+}
