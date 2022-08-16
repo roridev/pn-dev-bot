79c2b07f92335d83651abfb5f038a0d52ac6d3fb
roridev <prates.aliceic@gmail.com>
2022-08-16 15:42:09 -0300

    Add impl Queryable for `WaitingTag`

--- src/commands/slash/waiting.rs
+++ src/commands/slash/waiting.rs
@@ -1,3 +1,4 @@
+use crate::utils::formatting::Format;
 
 #[derive(poise::ChoiceParameter, Copy, Clone, Debug)]
 pub enum WaitingTag {
@@ -5,6 +6,17 @@ pub enum WaitingTag {
     Merge,
     InGameTest,
     Response,
+}
+
+impl Queryable for WaitingTag {
+    fn to_query(&self) -> String {
+        match self {
+            WaitingTag::Review => "Status: Awaiting Review".to_string(),
+            WaitingTag::Merge => "Status: Awaiting Merge".to_string(),
+            WaitingTag::InGameTest => "Status: Awaiting In Game Testing".to_string(),
+            WaitingTag::Response => "Status: Response".to_string(),
+        }
+    }
 }
 
 #[derive(poise::ChoiceParameter, Copy, Clone, Debug)]
